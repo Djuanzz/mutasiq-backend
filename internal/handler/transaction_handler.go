614bb5c873c231637248a6cdc6effa4ebb2f1957
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2026-04-07 21:42:10 +0700

    feat(transaction): add transaction handler

---
+++ internal/handler/transaction_handler.go
@@ -0,0 +1,66 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/Djuanzz/cashlens-backend/internal/service"
+	"github.com/Djuanzz/cashlens-backend/pkg/utils"
+	"github.com/gin-gonic/gin"
+)
+
+type TransactionHandler struct {
+	service *service.TransactionService
+}
+
+func NewTransactionHandler(ts *service.TransactionService) *TransactionHandler {
+	return &TransactionHandler{
+		service: ts,
+	}
+}
+
+func (h *TransactionHandler) ProcessTransactionFile(ctx *gin.Context) {
+
+	file, err := ctx.FormFile("file")
+	if err != nil {
+		utils.ErrorResponse(ctx, http.StatusBadRequest, "File is required")
+		return
+	}
+
+	uploadDir := "./uploads"
+	if err := os.MkdirAll(uploadDir, os.ModePerm); err != nil {
+		utils.ErrorResponse(ctx, http.StatusInternalServerError, "Failed to create directory")
+		return
+	}
+
+	filePath := filepath.Join(uploadDir, file.Filename)
+
+	// simpan file ke disk
+	if err := ctx.SaveUploadedFile(file, filePath); err != nil {
+		utils.ErrorResponse(ctx, http.StatusInternalServerError, "Failed to save file")
+		return
+	}
+
+	fmt.Println("File saved at:", filePath)
+
+	// baru kirim path ke service
+	err = h.service.ProcessTransactionFile(filePath)
+	if err != nil {
+		utils.ErrorResponse(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	utils.SuccessResponse(ctx, http.StatusOK, "File processed successfully", "")
+}
+
+func (h *TransactionHandler) BlueTakeJson(ctx *gin.Context) {
+	data, err := h.service.BlueTakeJson()
+	if err != nil {
+		utils.ErrorResponse(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	utils.SuccessResponse(ctx, http.StatusOK, "JSON transaction successfully", data)
+}
