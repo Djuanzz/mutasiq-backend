56496ac9067d81a7833f754a2eafe1e5aed3de11
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2026-04-07 21:42:28 +0700

    feat(transaction): register transaction routes

---
+++ internal/router/transaction_router.go
@@ -0,0 +1,16 @@
+package router
+
+import (
+	"github.com/Djuanzz/cashlens-backend/internal/handler"
+	"github.com/Djuanzz/cashlens-backend/internal/service"
+	"github.com/gin-gonic/gin"
+)
+
+func TransactionRouter(r *gin.RouterGroup) {
+	service := service.NewTransactionService()
+	handler := handler.NewTransactionHandler(service)
+	transaction := r.Group("/transaction")
+
+	transaction.POST("/process", handler.ProcessTransactionFile)
+	transaction.GET("/blue", handler.BlueTakeJson)
+}
