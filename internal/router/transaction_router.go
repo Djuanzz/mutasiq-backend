074a94d46f446c66ba49f64744ebc743a15b0436
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2026-04-28 15:05:39 +0700

    feat(router): wire repository and register crud routes

--- internal/router/transaction_router.go
+++ internal/router/transaction_router.go
@@ -2,14 +2,21 @@ package router
 
 import (
 	"github.com/Djuanzz/mutasiq-backend/internal/handler"
+	"github.com/Djuanzz/mutasiq-backend/internal/repository"
 	"github.com/Djuanzz/mutasiq-backend/internal/service"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
-func TransactionRouter(r *gin.RouterGroup) {
-	service := service.NewTransactionService()
-	handler := handler.NewTransactionHandler(service)
+func TransactionRouter(r *gin.RouterGroup, db *gorm.DB) {
+	repo := repository.NewTransactionRepository(db)
+	svc := service.NewTransactionService(repo)
+	handler := handler.NewTransactionHandler(svc)
 	transaction := r.Group("/transaction")
 
 	transaction.POST("/upload", handler.ProcessTransactionFile)
+	transaction.POST("/", handler.CreateTransaction)
+	transaction.GET("/", handler.GetAllTransactions)
+	transaction.PATCH("/:id", handler.UpdateTransaction)
+	transaction.DELETE("/:id", handler.DeleteTransaction)
 }
