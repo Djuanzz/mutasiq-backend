7dbbf1e72762b03785f052bd2ac29e5d3f6bec7c
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2026-04-28 15:05:15 +0700

    feat(model): add gorm tags, uuid primary key, and category field

--- internal/model/transaction.go
+++ internal/model/transaction.go
@@ -1,8 +1,12 @@
 package model
 
+import "github.com/google/uuid"
+
 type Transaction struct {
-	Date   string  `json:"date"`
-	Amount float64 `json:"amount"`
-	Type   string  `json:"type"`
-	Desc   string  `json:"desc"`
+	Id       uuid.UUID `gorm:"type:uuid;primaryKey" json:"id" form:"id"`
+	Date     string    `gorm:"type:varchar(255)" json:"date" form:"date"`
+	Amount   float64   `gorm:"type:decimal(10,2)" json:"amount" form:"amount"`
+	Type     string    `gorm:"type:varchar(255)" json:"type" form:"type"`
+	Desc     string    `gorm:"type:varchar(255)" json:"desc" form:"desc"`
+	Category *string   `gorm:"type:varchar(255)" json:"category" form:"category"`
 }
