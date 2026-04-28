21eb62abaacadcf9371c339462d9390bf938f62a
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2026-04-28 15:05:48 +0700

    feat(main): run db migration and pass db to router

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,15 +4,20 @@ import (
 	"fmt"
 
 	"github.com/Djuanzz/mutasiq-backend/internal/config"
+	"github.com/Djuanzz/mutasiq-backend/internal/model"
 	"github.com/Djuanzz/mutasiq-backend/internal/router"
+	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
 	fmt.Println("===== Cashlens Backend =====")
 	db := config.ConnectDatabase()
-
 	defer config.CloseDatabase(db)
 
-	r := router.SetupRouter()
+	if err := model.Migrate(db); err != nil {
+		panic("Failed to migrate database: " + err.Error())
+	}
+
+	r := router.SetupRouter(db)
 	r.Run(":5000")
 }
