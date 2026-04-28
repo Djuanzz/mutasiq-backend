44feec5822fc01d22cd9be7af13391e65a714484
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2026-04-28 15:05:44 +0700

    refactor(router): pass db instance to SetupRouter

--- internal/router/router.go
+++ internal/router/router.go
@@ -3,16 +3,17 @@ package router
 import (
 	"github.com/Djuanzz/mutasiq-backend/internal/middleware"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
-func SetupRouter() *gin.Engine {
+func SetupRouter(db *gorm.DB) *gin.Engine {
 	r := gin.Default()
 	r.Use(middleware.CORSMiddleware())
 
 	api := r.Group("/api")
 
 	HealthRouter(api)
-	TransactionRouter(api)
+	TransactionRouter(api, db)
 
 	return r
 }
