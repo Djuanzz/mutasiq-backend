f5498974ee5d2b4eaa2ede30808d18386acb2cfd
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2026-09-22 22:16:19 +0700

    feat(service): delete all categories

--- internal/service/category_service.go
+++ internal/service/category_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"errors"
+	"strings"
 
 	"github.com/Djuanzz/mutasiq-backend/internal/model"
 	"github.com/Djuanzz/mutasiq-backend/internal/repository"
@@ -17,6 +18,13 @@ func NewCategoryService(cr *repository.CategoryRepository) *CategoryService {
 }
 
 func (cs *CategoryService) CreateCategory(cm *model.Category) error {
+	cm.Name = strings.TrimSpace(cm.Name)
+	cm.Name = strings.ToLower(cm.Name)
+
+	if cm.Name == "" {
+		return errors.New("category name is required")
+	}
+
 	existing, err := cs.repo.FindByName(cm.Name)
 
 	if err != nil {
@@ -24,7 +32,7 @@ func (cs *CategoryService) CreateCategory(cm *model.Category) error {
 	}
 
 	if existing != nil {
-		return errors.New("Category already exists")
+		return errors.New("category already exists")
 	}
 
 	cm.Id = uuid.New()
@@ -33,4 +41,8 @@ func (cs *CategoryService) CreateCategory(cm *model.Category) error {
 
 func (cs *CategoryService) GetAllCategories() ([]model.Category, error) {
 	return cs.repo.GetAll()
+}
+
+func (cs *CategoryService) DeleteAllCategories() error {
+	return cs.repo.DeleteAll()
 }
