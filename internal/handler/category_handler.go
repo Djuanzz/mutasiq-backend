b7ef2742c8c389875e091381da9b8febc4fe7014
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2026-09-22 21:02:58 +0700

    feat(handler): create category

---
+++ internal/handler/category_handler.go
@@ -0,0 +1,34 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/Djuanzz/mutasiq-backend/internal/model"
+	"github.com/Djuanzz/mutasiq-backend/internal/service"
+	"github.com/Djuanzz/mutasiq-backend/pkg/utils"
+	"github.com/gin-gonic/gin"
+)
+
+type CategoryHandler struct {
+	service *service.CategoryService
+}
+
+func NewCategoryHandler(cs *service.CategoryService) *CategoryHandler {
+	return &CategoryHandler{service: cs}
+}
+
+func (ch *CategoryHandler) CreateCategory(ctx *gin.Context) {
+	var cm model.Category
+
+	if err := ctx.ShouldBind(&cm); err != nil {
+		utils.ErrorResponse(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if err := ch.service.CreateCategory(&cm); err != nil {
+		utils.ErrorResponse(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	utils.SuccessResponse(ctx, http.StatusCreated, "Category created successfully", cm)
+}
